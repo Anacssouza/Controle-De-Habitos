11138d1aa42a6437c5fa82ef5068647a453424fd
Ana Carolina Souza <89041008+Anacssouza@users.noreply.github.com>
2023-01-19 22:32:48 -0300

    Atualizando JS

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
 
-const data = {
-  run: ['01-01', '01-02', '01-06'],
-  water: ['01-01', '01-02', '01-03', '01-05', '01-06'],
-  food: ['01-01', '01-02', '01-04','01-06'],
-  study: ['01-01', '01-02', '01-03','01-04', '01-05', '01-06', '01-07']
+const button = document.querySelector('header button')
 
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+
+function add(){
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+
+  const dayExists = nlwSetup.dayExists(today)
+
+  if(dayExists){
+    alert('Você já adicionou essa data ❌')
+    return
+  }
+
+  alert('Adicionado com sucesso ✔')
+  nlwSetup.addDay(today)
 }
+
+function save(){
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))  //salvar no storage
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {} // recupera o que salvou antes no storage pra posteriormente colocar na tela (nesse caso), ou devolve um objeto vazio para não dar erro
 
 nlwSetup.setData(data)
 nlwSetup.load()
