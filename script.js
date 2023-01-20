fbc47f9ccdf5310d6b80f70c0b7edb6aea943fd3
Ana Carolina Souza <89041008+Anacssouza@users.noreply.github.com>
2023-01-19 21:41:06 -0300

    Iniciando JS

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('#form-habits')
+const nlwSetup = new NLWSetup(form)
+
+const data = {
+  run: ['01-01', '01-02', '01-06'],
+  water: ['01-01', '01-02', '01-03', '01-05', '01-06'],
+  food: ['01-01', '01-02', '01-04','01-06'],
+  study: ['01-01', '01-02', '01-03','01-04', '01-05', '01-06', '01-07']
+
+}
+
+nlwSetup.setData(data)
+nlwSetup.load()
